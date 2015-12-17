f7361818455bb8b103dc8a1a0cd08a4d37ebb48e
Athul Suresh <atulsuresh123@gmail.com>
2015-12-17 16:54:30 +0530

    inital updates

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/*** React App ***/
+
+
+
+var HomeSinglePost = React.createClass({
+
+	render:function(){
+
+		return(
+					<h1>hee</h1>
+			);
+	}
+	
+
+
+});
+
+React.renderComponent(React.DOM.h1(null,'Hellow Word'), document.getElementById('rapp'));
